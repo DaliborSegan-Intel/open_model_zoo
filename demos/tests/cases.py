dcc7761322300826e22398bb0fe0c9d52a1b3f88
Dalibor Segan <dalibor.segan@intel.com>
2021-01-21 00:30:44 -0800

    Social Distance Demo - WIP
     * Next steps: refactor code to minimize code copying

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -327,6 +327,17 @@ NATIVE_DEMOS = [
         single_option_cases('-m_td', ModelArg('text-detection-0003'), ModelArg('text-detection-0004')),
         single_option_cases('-m_tr', None, ModelArg('text-recognition-0012')),
     )),
+
+    NativeDemo(subdirectory='social_distance_demo',
+            device_keys=['-d_det', '-d_reid'],
+            test_cases=combine_cases(
+        TestCase(options={'-no_show': None,
+            **MONITORS,
+            '-i': DataPatternArg('person-detection-retail')}),
+        TestCase(options={'-m_det': ModelArg('person-detection-retail-0013')}),
+        single_option_cases('-m_reid',
+            ModelArg('person-reidentification-retail-0300')),
+    )),
 ]
 
 PYTHON_DEMOS = [
